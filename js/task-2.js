73d8df2b3f06a4bbb476be4c2d499c397aa1452a
peychma <mariapei4eva@gmail.com>
2023-10-29 21:48:57 +0200

    .

--- js/task-2.js
+++ js/task-2.js
@@ -2,10 +2,6 @@ function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
-getShippingMessage();
 
 console.log(getShippingMessage("Australia", 120, 50));
 console.log(getShippingMessage("Germany", 80, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,6 @@
 function getElementWidth(content, padding, border) {
 return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-getElementWidth();
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
